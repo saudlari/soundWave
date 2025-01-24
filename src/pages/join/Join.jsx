4ef92f7fcfdb27015a05712dde0fac9cf0189769
Tetiana-BCN <tetiana.v.a@gmail.com>
2025-01-24 15:30:14 +0100

    create text and form for Join

---
+++ src/pages/join/Join.jsx
@@ -0,0 +1,35 @@
+import styles from './Join.css'
+import './header'
+import './footer'
+
+function Join () {
+  
+    return (
+<main className={styles.joinContainer}>
+<section>
+    <h1>
+        Join the <span className={styles.spanJoin}>fun.</span>
+    </h1>
+
+  <div className={styles.formLogin}>
+    <label for="username"><b>Name:</b></label>
+    <input type="text" name="uname" required></input>
+
+    <label for="email"><b>Email:</b></label>
+    <input type="email" name="email" required></input>
+
+    <label for="psw"><b>Password:</b></label>
+    <input type="password" name="psw" required></input>
+
+    <button type="submit">Join Now</button>
+    
+  </div>
+</section>
+
+
+
+<main/>
+    )
+};
+
+export default Join;

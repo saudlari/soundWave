01f13d7b90d83a44dc969d283e440402ada4747b
kat-lev <kat@katleverton.com>
2025-02-03 09:54:18 +0100

    add tests x2 for username and email address

--- test/validation.test.jsx
+++ test/validation.test.jsx
@@ -1,19 +1,45 @@
 import { describe, it, expect } from "vitest";
 
-const passwordOk = (password) => {
+const usernameOK = (username) => {
+    const validUsernameLength = username.length >= 3 && username.length <= 20;
+    const validUsernameCharacters = /^[a-zA-Z0-9_]+$/.test(username);
+  return validUsernameLength && validUsernameCharacters;
+}
 
+describe("Username validation", () => {
+  it ("is a valid username", () => {
+    expect(usernameOK("Hola_test44")).toBe(true);
+    expect(usernameOK("Hola!60")).toBe(false);
+  })
+})
+
+const emailOK = (email) => {
+    const containsSymbol = email.includes('@');
+    const noSpaces = !/\s/.test(email);
+    const containsDomain = /@.+/.test(email);
+return containsSymbol && noSpaces && containsDomain;
+}
+
+describe("Email validation", () => {
+  it ("is a valid email address", () => {
+    expect(emailOK("hola@test.com")).toBe(true);
+    expect(emailOK("holatest@")).toBe(false);
+    expect(emailOK("hola.test.com")).toBe(false);
+    expect(emailOK("hola @test.com")).toBe(false);
+  })
+})
+
+const passwordOk = (password) => {
     const validLength = password.length >= 8 && password.length <= 16;
     const numericCharacter = /[0-9]/.test(password);
     const lowerCase = /[a-z]/.test(password);
     const upperCase = /[A-Z]/.test(password);
     const alphanumeric = /^[a-zA-Z0-9]+$/.test(password);
-
 return validLength && numericCharacter && lowerCase && upperCase && alphanumeric;
 };
 
-
-describe("Password Validation", () => {
-  it("A valid password", () => {
+describe("Password validation", () => {
+  it("is a valid password", () => {
     expect(passwordOk("Password123")).toBe(true);
   });
 
